1251e2ddf7b323c75a2b03fd8515328988cb9c43
Ryan Wang <i@ryanc.cc>
2024-06-07 12:23:42 +0800

    feat: watch href
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- ui/src/components/HyperlinkInlineView.vue
+++ ui/src/components/HyperlinkInlineView.vue
@@ -1,7 +1,21 @@
 <script lang="ts" setup>
+import type { HyperlinkInlineCard } from '@halo-dev/hyperlink-card';
 import { nodeViewProps, NodeViewWrapper } from '@halo-dev/richtext-editor';
+import { ref, watch } from 'vue';
 
-defineProps(nodeViewProps);
+const props = defineProps(nodeViewProps);
+
+const cardRef = ref<InstanceType<typeof HyperlinkInlineCard> | null>();
+
+watch(
+  () => props.node.attrs.href,
+  (value) => {
+    if (value && cardRef.value) {
+      cardRef.value.href = value;
+      cardRef.value.fetchSiteData();
+    }
+  }
+);
 </script>
 
 <template>
@@ -11,6 +25,7 @@ defineProps(nodeViewProps);
     :class="{ 'hyperlink-ring-1': selected }"
   >
     <hyperlink-inline-card
+      ref="cardRef"
       class="hyperlink-pointer-events-none hyperlink-select-none"
       :href="node.attrs.href"
       :theme="node.attrs.theme"
--- ui/src/components/HyperlinkView.vue
+++ ui/src/components/HyperlinkView.vue
@@ -1,7 +1,21 @@
 <script lang="ts" setup>
+import type { HyperlinkCard } from '@halo-dev/hyperlink-card';
 import { nodeViewProps, NodeViewWrapper } from '@halo-dev/richtext-editor';
+import { ref, watch } from 'vue';
 
-defineProps(nodeViewProps);
+const props = defineProps(nodeViewProps);
+
+const cardRef = ref<InstanceType<typeof HyperlinkCard> | null>();
+
+watch(
+  () => props.node.attrs.href,
+  (value) => {
+    if (value && cardRef.value) {
+      cardRef.value.href = value;
+      cardRef.value.fetchSiteData();
+    }
+  }
+);
 </script>
 
 <template>
@@ -11,6 +25,7 @@ defineProps(nodeViewProps);
     :class="{ 'hyperlink-rounded-xl hyperlink-ring-1': selected }"
   >
     <hyperlink-card
+      ref="cardRef"
       class="hyperlink-pointer-events-none hyperlink-select-none"
       :href="node.attrs.href"
       :theme="node.attrs.theme"
