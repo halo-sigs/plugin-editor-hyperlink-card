6a895337829b7ee5b836a29c09414f27e312ae85
guqing <i@guqing.email>
2024-07-02 17:16:19 +0800

    chore: remove deprecated plugin wrapper

--- src/main/java/run/halo/editor/hyperlink/HyperLinkHeadProcessor.java
+++ src/main/java/run/halo/editor/hyperlink/HyperLinkHeadProcessor.java
@@ -2,7 +2,6 @@ package run.halo.editor.hyperlink;
 
 import java.util.Properties;
 import lombok.RequiredArgsConstructor;
-import org.pf4j.PluginWrapper;
 import org.springframework.stereotype.Component;
 import org.springframework.util.PropertyPlaceholderHelper;
 import org.thymeleaf.context.ITemplateContext;
@@ -10,6 +9,7 @@ import org.thymeleaf.model.IModel;
 import org.thymeleaf.model.IModelFactory;
 import org.thymeleaf.processor.element.IElementModelStructureHandler;
 import reactor.core.publisher.Mono;
+import run.halo.app.plugin.PluginContext;
 import run.halo.app.theme.dialect.TemplateHeadProcessor;
 
 @Component
@@ -19,7 +19,7 @@ public class HyperLinkHeadProcessor implements TemplateHeadProcessor {
     static final PropertyPlaceholderHelper PROPERTY_PLACEHOLDER_HELPER =
         new PropertyPlaceholderHelper("${", "}");
 
-    private final PluginWrapper pluginWrapper;
+    private final PluginContext pluginContext;
 
     @Override
     public Mono<Void> process(ITemplateContext context, IModel model,
@@ -32,7 +32,7 @@ public class HyperLinkHeadProcessor implements TemplateHeadProcessor {
     private String hyperlinkCardComponentScript() {
 
         final Properties properties = new Properties();
-        properties.setProperty("version", pluginWrapper.getDescriptor().getVersion());
+        properties.setProperty("version", pluginContext.getVersion());
 
         return PROPERTY_PLACEHOLDER_HELPER.replacePlaceholders("""
             <!-- plugin-editor-hyperlink-card start -->
