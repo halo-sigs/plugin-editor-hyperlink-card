bed0b96b6c01f905ef5a11988ea6d10df9091778
Ryan Wang <i@ryanc.cc>
2025-12-27 15:19:06 +0800

    Add icon upload option for inline hyperlinks

--- ui/src/components/HyperlinkPropsBubbleButton.vue
+++ ui/src/components/HyperlinkPropsBubbleButton.vue
@@ -89,12 +89,25 @@ const isInline = computed(() => {
         <Input v-model="customTitle" auto-focus label="自定义标题" />
         <Input v-if="!isInline" v-model="customDescription" label="自定义描述" />
         <FormKit
+          v-if="!isInline"
           v-model="customImage"
           :classes="{
             outer: ':uno: !pt-0',
           }"
           type="attachment"
           help="自定义图片"
+        ></FormKit>
+
+        <FormKit
+          v-else
+          v-model="customImage"
+          :classes="{
+            outer: ':uno: !pt-0',
+          }"
+          format="dataurl"
+          value-only
+          type="iconify"
+          help="自定义图标"
         ></FormKit>
       </div>
     </template>
